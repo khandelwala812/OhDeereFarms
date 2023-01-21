fbc3a0342a520586c012d387d01ffa08222f289b
Andy <Andy_lam20@outlook.com>
2023-01-20 23:50:51 -0500

    Made player movement

--- venv/src/Main.py
+++ venv/src/Main.py
@@ -11,14 +11,40 @@ running = True
 #############################-Load Map-######################################
 
 #############################-Camera-######################################
-PLAYER_SPRITE = ''
+PLAYER_SPRITE = 'assets\deer.png'
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
-        self.image = pygame.image.load('')
+        self.image = pygame.image.load(PLAYER_SPRITE)
+        self.rect = self.image.get_rect(center = pos)
+        self.direction = pygame.math.Vector2()
+        self.speed = 10
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w]:
+            self.direction.y = -1
+        elif keys[pygame.K_s]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_d]:
+            self.direction.x = 1
+        elif keys[pygame.K_a]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def update(self):
+        self.input()
+        self.rect.center += self.direction * self.speed
+
 # Setup
 camera_group = pygame.sprite.Group()
+Player((635,350), camera_group)
 
 #############################-Game Loop-######################################
 while running:
@@ -27,7 +53,9 @@ while running:
     for event in pygame.event.get():
         if event.type ==pygame.QUIT:
             running = False
+        
+    camera_group.update()
+    camera_group.draw(screen)
     
-    screen.fill((255,255,255))
     pygame.display.flip()
     clock.tick(60)
