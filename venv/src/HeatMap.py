245613b30c5c8a9a56f2a028df3437a51546cca8
StevenSchonlau <sschonla@purdue.edu>
2023-01-21 09:41:11 -0500

    Added heatmap visual function

--- venv/src/HeatMap.py
+++ venv/src/HeatMap.py
@@ -1,5 +1,8 @@
 #returns heat map based on input of f for fertilizer, t for tillage, s for same crop
-def heatmap(levelType):
+import pygame.display
+
+
+def heatmap5(levelType):
     heatmap = [[],[]]
     #gives weights for color
     for i in tiles:
@@ -17,3 +20,37 @@ def heatmap(levelType):
             j += 5
             i += 5
     return heatmap
+#returns average value based on input of f for fertilizer, t for tillage, s for same crop
+def heatmap(levelType):
+    tileSum = 0
+    #gives weights for color
+    for i in tiles:
+        for j in tiles[i]:
+            if levelType == "f":
+                tileSum += tiles[i][j].fertilizer
+            elif levelType == "t":
+                tileSum += tiles[i][j].tillage
+            elif levelType == "s":
+                tileSum += tiles[i][j].sameCrop
+
+    return tileSum/100
+
+def showHeatMap(levelType):
+    width = screen.get_width()
+    height = screen.get_height()
+    largeText =pygame.font.SysFont("comicsansms", 115)
+    TextPause = text_objects("Paused", largeText)
+    TextPause.center = ((width/2),(height/2))
+    gameDisplay.blit(TextPause)
+    surfaceHM = pygame.display.set_mode(100, 100)
+    heatmapMatrix = heatmap5(levelType)
+    for i in heatmapMatrix:
+        for j in heatmapMatrix[i]:
+            if heatmapMatrix[i][j] < 0.5:
+                color = (0, 0, 255*(1-heatmapMatrix[i][j]))
+            else:
+                color = (255*(heatmapMatrix[i][j]), 0, 0)
+            pygame.draw.rect(surfaceHM, color, pygame.rect(i*5, j*5, 5, 5))
+
+    #button("Continue",150,450,100,50,green,bright_green,unpause)
+    #button("Quit",550,450,100,50,red,bright_red,quitgame)
