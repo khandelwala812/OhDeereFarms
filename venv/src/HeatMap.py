3f5624113695fcd4a2c2d69672cc4f549ec6f7b6
StevenSchonlau <sschonla@purdue.edu>
2023-01-21 09:17:33 -0500

    created heatmap function

--- venv/src/HeatMap.py
+++ venv/src/HeatMap.py
@@ -1,11 +1,19 @@
-heatmap = [[],[]]
-#gives weights for color
-for i in tiles:
-    for j in tiles[i]:
-        tileSum = 0
-        for k in range(5):
-            for l in range(5):
-                tileSum += tiles[k][l].fertilizer
-        heatmap[i/5][j/5] = tileSum/25
-        j += 5
-        i += 5
+#returns heat map based on input of f for fertilizer, t for tillage, s for same crop
+def heatmap(levelType):
+    heatmap = [[],[]]
+    #gives weights for color
+    for i in tiles:
+        for j in tiles[i]:
+            tileSum = 0
+            for k in range(5):
+                for l in range(5):
+                    if levelType == "f":
+                        tileSum += tiles[k][l].fertilizer
+                    elif levelType == "t":
+                        tileSum += tiles[k][l].tillage
+                    elif levelType == "s":
+                        tileSum += tiles[k][l].sameCrop
+            heatmap[i/5][j/5] = tileSum/25
+            j += 5
+            i += 5
+    return heatmap
