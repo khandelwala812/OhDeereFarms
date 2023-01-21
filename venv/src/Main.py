b9c0c68fc02111a55c6f264ac68202b773b1fa40
Andy <Andy_lam20@outlook.com>
2023-01-21 12:09:29 -0500

    added getCords

--- venv/src/Main.py
+++ venv/src/Main.py
@@ -22,37 +22,34 @@ class Player(pygame.sprite.Sprite):
         super().__init__(group)
         self.image = pygame.image.load(PLAYER_SPRITE)
         self.rect = self.image.get_rect(center=pos)
-        self.direction = pygame.math.Vector2()
+        self.pos_x = pos[0]
+        self.pos_y = pos[1]
         self.speed = 10
 
     def input(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_w]:
-            self.direction.y = 1
-            self.getPos()
+            self.pos_y += self.speed
+            self.getCords()
         elif keys[pygame.K_s]:
-            self.direction.y = -1
-            self.getPos()
-        else:
-            self.direction.y = 0
-
-        if keys[pygame.K_d]:
-            self.direction.x = -1
-            self.getPos()
+            self.pos_y -= self.speed
+            self.getCords()
+        elif keys[pygame.K_d]:
+            self.pos_x += self.speed
+            self.getCords()
         elif keys[pygame.K_a]:
-            self.direction.x = 1
-            self.getPos()
-        else:
-            self.direction.x = 0
+            self.pos_x -= self.speed
+            self.getCords()
 
     def update(self):
         self.input()
-        self.rect.center += self.direction * self.speed
 
-    def getPos(self):
-        print(f"x: {self.rect.centerx}")
-        print(f"y: {self.rect.centery}")
+    def printCords(self):
+        print(f"({self.pos_x}, {self.pos_y})")
+
+    def getCords(self):
+        return self.pos_x, self.pos_y
 
 class CameraGroup(pygame.sprite.Group):
     def __init__(self):
