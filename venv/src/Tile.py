42c5b32f0cd1bf5dfb0189797c0ff79dcb196ee7
khandelwala812 <avikwal123@gmail.com>
2023-01-20 23:11:54 -0500

    Creat crop and tile classes

---
+++ venv/src/Tile.py
@@ -0,0 +1,33 @@
+CONDITIONS = ["empty", "tilled", "seed", "seedling", "hapling", "harvest"]
+
+
+class Tile:
+    def __int__(self, crop):
+        self._crop = crop
+        self._growthTime = crop.growthTime
+        self._condition = 0
+        self._fertilizerLevel = 0
+
+    @property
+    def growthTime(self):
+        return self._growthTime - self._fertilizerLevel * self._growthTime
+
+    @growthTime.setter
+    def growthTime(self, time):
+        self._growthTime = time
+
+    @property
+    def condition(self):
+        return CONDITIONS[self._condition]
+
+    @condition.setter
+    def condition(self, condition):
+        self._condition = condition
+
+    @property
+    def fertilizerLevel(self):
+        return self._fertilizerLevel
+
+    @fertilizerLevel.setter
+    def fertilizerLevel(self, level):
+        self._fertilizerLevel = level
